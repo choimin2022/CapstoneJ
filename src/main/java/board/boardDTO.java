a15565393df27f4bacf3e8fe36b62b8f2b398c81
choimin2022 <101938357+choimin2022@users.noreply.github.com>
2023-05-14 16:02:55 +0900

    2023/05/14 최민기 DTO
    최민기

---
+++ src/main/java/board/boardDTO.java
@@ -0,0 +1,27 @@
+package board;
+
+public class boardDTO {
+	private int b_num;
+	private String b_title; 
+	private String b_url;
+	public int getB_num() {
+		return b_num;
+	}
+	public void setB_num(int b_num) {
+		this.b_num = b_num;
+	}
+	public String getB_title() {
+		return b_title;
+	}
+	public void setB_title(String b_title) {
+		this.b_title = b_title;
+	}
+	public String getB_url() {
+		return b_url;
+	}
+	public void setB_url(String b_url) {
+		this.b_url = b_url;
+	}
+	
+	
+}
