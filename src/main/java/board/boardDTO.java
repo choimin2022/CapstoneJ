874abecf9d7a71ed9e11ef157bf9638775595875
choimin2022 <101938357+choimin2022@users.noreply.github.com>
2023-05-23 13:33:13 +0900

    최민기 게시판 DTO

--- src/main/java/board/boardDTO.java
+++ src/main/java/board/boardDTO.java
@@ -2,13 +2,23 @@ package board;
 
 public class boardDTO {
 	private int b_num;
-	private String b_title; 
-	private String b_url;
+	private String u_id;
+	private String b_title;
+	private String b_content;
+	private int b_like;
+	private int b_check;
+	private String date;
 	public int getB_num() {
 		return b_num;
 	}
 	public void setB_num(int b_num) {
 		this.b_num = b_num;
+	}
+	public String getU_id() {
+		return u_id;
+	}
+	public void setU_id(String u_id) {
+		this.u_id = u_id;
 	}
 	public String getB_title() {
 		return b_title;
@@ -16,12 +26,31 @@ public class boardDTO {
 	public void setB_title(String b_title) {
 		this.b_title = b_title;
 	}
-	public String getB_url() {
-		return b_url;
+	public String getB_content() {
+		return b_content;
 	}
-	public void setB_url(String b_url) {
-		this.b_url = b_url;
+	public void setB_content(String b_content) {
+		this.b_content = b_content;
 	}
+	public int getB_like() {
+		return b_like;
+	}
+	public void setB_like(int b_like) {
+		this.b_like = b_like;
+	}
+	public int getB_check() {
+		return b_check;
+	}
+	public void setB_check(int b_check) {
+		this.b_check = b_check;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
 	
 	
 }
